1546fa1b3df1be3b8b03365c33a9d99b44ba37f2
Rafael Vazquez <rafa_a_vazquez@hotmail.com>
2024-02-25 17:52:18 -0300

    components created with styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SmallCardComponent } from "./components/small-card/small-card.component";
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
+import { BigCardComponent } from "./components/big-card/big-card.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, SmallCardComponent, MenuTitleComponent, MenuBarComponent, BigCardComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
